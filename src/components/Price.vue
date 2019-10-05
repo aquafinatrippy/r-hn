1fe53c0e77ffc533e0fb208d071f4e22304350b4
Tanel Tekko <taneldeviis@hotmail.com>
2019-10-05 14:27:58 +0300

    File add dummy, empty object component

---
+++ src/components/Price.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <table>
+      <thead>
+        <tr>
+          <th>Wood type</th>
+          <th>4meter sill price</th>
+          <th>Sills total price</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>Timber</td>
+          <td>24&euro;</td>
+          <td>62&euro;</td>
+        </tr>
+        <tr>
+          <td>Timber</td>
+          <td>24&euro;</td>
+          <td>62&euro;</td>
+        </tr>
+        <tr>
+          <td>Timber</td>
+          <td>24&euro;</td>
+          <td>62&euro;</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Price"
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="home" class="align-wrapper">
+  <body class="container">
+    <div class="row">
+      <form class="col s12">
+        <div class="row">
+          <label>Input file</label>
+          <div class="file-field input-field">
+            <div class="btn">
+              <span>Browse</span>
+              <input type="file" />
+            </div>
+
+            <div class="file-path-wrapper">
+              <input class="file-path validate" type="text" placeholder="Upload file" />
+            </div>
+          </div>
+        </div>
+        <a class="btn" href="/price">Submit</a>
+      </form>
+    </div>
+  </body>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
+
+<style lang="scss" scoped>
+#home {
+  height: 100vh;
+  justify-content: center;
+  display: flex;
+  align-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
